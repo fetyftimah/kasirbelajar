2fd682230e2800774036e8d263829544d13b224f
fetyftimah <fetyf115@gmail.com>
2023-08-24 23:00:17 +0800

    first

---
+++ database/migrations/2023_08_24_141505_transaksi.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no_transaksi');
+            $table->date('tgl_transaksi')->nullable();
+            $table->integer('diskon')->nullable();
+            $table->bigInteger('total_bayar')->nullable();
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_transaksi');
+    }
+};
